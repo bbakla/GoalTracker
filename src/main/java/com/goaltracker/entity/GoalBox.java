b93cd3cb84c91eb9fe174464b71eb6bea69cade6
Baris Bakla <baris.bakla@evosoft.com>
2018-07-16 12:56:22 +0200

    unnecessary import removed

--- src/main/java/com/goaltracker/entity/GoalBox.java
+++ src/main/java/com/goaltracker/entity/GoalBox.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
